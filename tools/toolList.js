d69fabb6e0576610fb33effcc46d090318eae9d2
Jordan Wambaugh <jordan@wambaugh.org>
2012-01-09 18:02:42 -0800

    Work on tools platform

--- tools/toolList.js
+++ tools/toolList.js
@@ -3,7 +3,7 @@
  */
 
 
-
+console.log('this is loaded');
 
 ToolUI.toolList = [
                    {name:'Entity Editor',file:'entityEditor/entityEditor.js'}
@@ -11,3 +11,20 @@ ToolUI.toolList = [
                    ]
 
 
+var select = document.createElement('select');
+
+
+var option = document.createElement('option');
+option.value = "";
+option.innerHTML = '-- Select A Tool --';
+select.appendChild(option);
+
+for(var x=0;x<ToolUI.toolList.length;x++){
+	var option = document.createElement('option');
+	option.value = ToolUI.toolList[x].file;
+	option.innerHTML = ToolUI.toolList[x].name;
+	select.appendChild(option);
+}
+
+
+ToolUI.render('header',select);
